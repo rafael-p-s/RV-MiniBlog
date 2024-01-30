6b181a921916a988628711ee27190fba93654d2b
rafael-p-s <rafaelsky321@gmail.com>
2024-01-30 15:35:01 -0300

    Criado o useFetchDocuments.jsx para buscar os posts, do usuário logado, dentro do FireBase.

---
+++ src/hooks/useFetchDocuments.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  where,
+} from "firebase/firestore"; //referente aos comandos do banco de dados
+
+//Passando os parametros para fazer a conexão, a busca das tags e validação de usuário.
+export function useFetchDocuments(docCollection, search = null, uid = null) {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  //deal with memory leak
+  const [cancelled, setCancelled] = useState(false);
+
+  useEffect(() => {
+    async function loadData() {
+      if (cancelled) return;
+      setLoading(true);
+      const collectionRef = await collection(db, docCollection);
+
+      try {
+        let callQuery;
+        callQuery = await query(collectionRef, orderBy("createAt", "desc")); //está chamando por ordem de validade de criação, mais novos primeiro.
+
+        //mapear os dados
+        await onSnapshot(callQuery, (querySnapshot) => {
+          //executar uma função para vir com os docs do FireBase
+          setDocuments(
+            //fazendo um mapeamento, para acessar os docs para verificar os dados que veio
+            querySnapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }))
+          );
+        });
+
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setError(error.message);
+
+        setLoading(false);
+      }
+      loadData();
+    }
+  }, [docCollection, search, uid, cancelled]);
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return { documents, loading, error };
+}
