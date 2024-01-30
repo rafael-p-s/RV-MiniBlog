b03a715174f838f0c77f70ad37f5536bbdfb1980
rafael-p-s <rafaelsky321@gmail.com>
2024-01-30 15:15:00 -0300

    Home recebeu configurações CSS para exibir as postagens

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,40 @@
 import styles from "./Home.module.css";
 
+//hooks
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
 export function Home() {
+  const [query, setQuery] = useState("");
+  const [posts] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <>
-      <h1>Home</h1>
-      
-    </>
+    <div className={styles.home}>
+      <h1>Posts mais recentes:</h1>
+      <hr />
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input type="text" placeholder="Ou busque por tags..." />
+        <button
+          className="btn btn-dark"
+          onChange={(e) => setQuery(e.target.value)}
+        >
+          Pesquisar
+        </button>
+      </form>
+      <div>
+        <h1>Posts...</h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
